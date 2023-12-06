6ab2c1eaf13e4f37043a6634c99c638f8aed6b1d
Yetu <wdhuang927@gmail.com>
2023-12-06 17:03:17 +0800

    Create add.R

---
+++ R/add.R
@@ -0,0 +1,19 @@
+# 打开文件开始编写，写入下列内容
+#' Title
+#'
+#' Returns the sum of two numbers
+#'
+#' @param a Number one.
+#' @param b Number two.
+#'
+#' @return Sum of two number.
+#' @export
+#'
+#' @examples
+#' library(rPackageTutorial)
+#' add(1, 2)
+add<-function(a,b) {
+  a+b
+}
+
+
